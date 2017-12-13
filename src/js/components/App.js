21179644a20c06a28b1b21c68da15e944cbdf89f
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-12-13 17:42:27 +0900

    Add Editor component

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 
+import Editor from "./Editor";
+
 export default class App extends Component {
   render() {
-    return <div>App Component.</div>;
+    return (
+      <div>
+        App Component.
+        <Editor />
+      </div>
+    );
   }
 }
