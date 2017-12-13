55cf834b1e68a360492de9d77e4ac39cd06dc226
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2017-12-13 18:00:31 +0900

    Add react-codemirro2

--- src/js/components/Editor.js
+++ src/js/components/Editor.js
@@ -1,7 +1,39 @@
 import React, { Component } from "react";
+import { Controlled as CodeMirror } from "react-codemirror2";
+
+import { extraKeys } from "../keymap";
+
+require("codemirror/mode/markdown/markdown");
+require("codemirror/addon/edit/continuelist");
 
 export default class Editor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "# initial value"
+    };
+  }
+
+  options = {
+    mode: "markdown",
+    theme: "3024-day",
+    lineNumbers: true,
+    extraKeys
+  };
+
   render() {
-    return <div>Editor Component</div>;
+    return (
+      <div>
+        Editor Component
+        <CodeMirror
+          value={this.state.value}
+          options={this.options}
+          onBeforeChange={(editor, data, value) => {
+            this.setState({ value });
+          }}
+          onChange={(editor, data, value) => {}}
+        />
+      </div>
+    );
   }
 }
---
+++ src/js/keymap.js
@@ -0,0 +1,12 @@
+export const extraKeys = {
+  // "Ctrl-Enter": () => {
+  //   this.handleSubmit();
+  // },
+  // "Cmd-Enter": () => {
+  //   this.handleSubmit();
+  // },
+  Esc: () => {
+    document.activeElement.blur();
+  },
+  Enter: "newlineAndIndentContinueMarkdownList"
+};
